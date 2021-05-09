26a57319a376c450331ef1508d0462a2381844df
Hermes Orosco <hermesorosco2010@gmail.com>
2021-05-08 22:04:32 -0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,13 @@ CWD=$PWD
 #sudo make install
 #cd $CWD
 echo -e "\n\nBuilding projects...\n\n"
+mkdir -p ./shared/obj
+cd $PWD
+make -C ./Discordiador
 make -C ./Discordiador Discordiador
+make -C ./i-MongoStore
 make -C ./i-MongoStore i-MongoStore
+make -C ./Mi-RAM\ HQ
 make -C ./Mi-RAM\ HQ Mi-RAM-HQ
+
 echo -e "\n\nDeploy done!\n\n"
