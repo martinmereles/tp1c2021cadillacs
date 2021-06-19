4d9a604d0329a873213d763a9da3bc2f28054349
Javier Happel <happeljavierleonardo@gmail.com>
2021-06-19 19:26:36 -0300

    agregado de tareas

--- i-MongoStore/src/servidor_i_mongo_store.c
+++ i-MongoStore/src/servidor_i_mongo_store.c
@@ -234,19 +234,344 @@ void recibir_tarea(char* payload){
                 break;
             }
             case GENERAR_COMIDA:{
+                char ** bloques_usables;
+                int cantidad_de_bloques;
+                char * path = crear_path_absoluto("/Files/Comida.ims");
+                
+                if(!existe_archivo(path)){
+                    //creando archivo Comida.ims de 0
+                    int resto = atoi(nombre_parametros[1])%super_bloque.blocksize;
+                    cantidad_de_bloques=atoi(nombre_parametros[1])/super_bloque.blocksize;
+                    if(resto!=0){
+                        cantidad_de_bloques++;
+                    }
+                    printf("cantidad bloques %d\n",cantidad_de_bloques);
+                    char * bloques_config;
+                    //string_append(&bloques_config,"[");
+                    crear_archivo(path);
+                    tarea_config_file = config_create(path);
+                    bloques_usables = bits_libres(&bitmap,cantidad_de_bloques);
+
+                    //modifica el array para guardarlo como char * en el config
+                    bloques_config = array_block_to_string(bloques_usables,cantidad_de_bloques);
+                    printf("bloques a config: %s\n", bloques_config);
+                    
+                    for(int i = 0; bloques_usables[i]!=NULL;i++){
+                        ocupar_bit(&bitmap, atoi(bloques_usables[i]));
+                        int offset = atoi(bloques_usables[i])*super_bloque.blocksize;
+                        if(i==cantidad_de_bloques-1){
+                            //string_append(&bloques_config,bloques_usables[i]);
+                            char * fill = string_repeat('C',resto);
+                            memcpy(blocksmap+offset,fill,resto);
+                        }else{
+                            //string_append(&bloques_config,bloques_usables[i]);
+                            //string_append(&bloques_config,",");
+                            
+                            char * fill = string_repeat('C',(int)super_bloque.blocksize);
+                            memcpy(blocksmap+offset,fill,super_bloque.blocksize);
+                        }
+                    }
+                    //string_append(&bloques_config,"]");
+                    msync(blocksmap, blocks_stat.st_size, MS_SYNC);
+                    config_set_value(tarea_config_file,"CARACTER_LLENADO","C");
+                    set_tarea_config(
+                        tarea_config_file,
+                        nombre_parametros[1],
+                        string_itoa(cantidad_de_bloques),
+                        bloques_config,
+                        "QWERTY"
+                    );
+                    config_save(tarea_config_file);
+                    printf("caracter llenado: %s\n",config_get_string_value(tarea_config_file,"CARACTER_LLENADO"));
+                }else{
+                    //Ya existiendo Comida.ims
+                    tarea_config_file = config_create(path);
+                    leer_tarea_config(tarea_config_file);
+                    char * caracter_llenado = tarea_config.caracter_llenado;
+                    char ** lista_bloques = string_get_string_as_array(tarea_config.blocks);
+                    char * bloques_config;
+                    int size_a_modificar = atoi(nombre_parametros[1]);
+
+                    int resto_anterior = tarea_config.size%super_bloque.blocksize;
+                    printf("resto anterior%d\n",resto_anterior);
+                    int cantidad_repetir;
+
+                    //En caso de que el ultimo bloque no este lleno, decide si 
+                    //hay que usar solo parte de el o
+                    //utilizar todo el bloque y solicitar otro
+                    if((super_bloque.blocksize-resto_anterior)>size_a_modificar){
+                        cantidad_repetir = size_a_modificar;
+                    }else{
+                        cantidad_repetir = super_bloque.blocksize-resto_anterior;
+                    }
+                    if(resto_anterior!=0){
+                        char * fill = string_repeat(*caracter_llenado,cantidad_repetir);
+                        char * ultimo_bloque_anterior = lista_bloques[tarea_config.block_count-1];
+                        printf("bloque numero:%s\n",ultimo_bloque_anterior);
+                        int ultimo_bloque_offset = atoi(ultimo_bloque_anterior)*super_bloque.blocksize;
+                        memcpy(blocksmap+ultimo_bloque_offset+ resto_anterior,fill,cantidad_repetir);
+                        size_a_modificar-=(cantidad_repetir);
+                    }
+
+                    int resto = size_a_modificar%super_bloque.blocksize;
+                    printf("resto actual:%d\n",resto);
+                    cantidad_de_bloques=size_a_modificar/super_bloque.blocksize;
+                    if(resto!=0){
+                        cantidad_de_bloques++;
+                    }
+                    printf("cantidad bloques %d\n",cantidad_de_bloques);
+                   
+                    //solicita una lista de bloques disponibles y los setea en memoria
+                    bloques_usables = bits_libres(&bitmap,cantidad_de_bloques);
+
+                    //une la lista de bloques del config con los bloques nuevos a usar
+                    bloques_config=array_two_block_to_string(lista_bloques,bloques_usables);
+                    printf("bloques a config: %s\n", bloques_config);
+
+                    for(int i = 0; bloques_usables[i]!=NULL;i++){
+                        int offset = atoi(bloques_usables[i])*super_bloque.blocksize;
+                        if(i==cantidad_de_bloques-1){
+                            char * fill = string_repeat(*caracter_llenado,resto);
+                            memcpy(blocksmap+offset,fill,strlen(fill));
+                        }else{
+                            char * fill = string_repeat(*caracter_llenado,(int)super_bloque.blocksize);
+                            memcpy(blocksmap+offset,fill,strlen(fill));
+                        }
+                    }
+                    msync(blocksmap, blocks_stat.st_size, MS_SYNC);
+                    eliminar_keys_tarea(tarea_config_file);
+                    set_tarea_config(
+                        tarea_config_file,
+                        string_itoa(tarea_config.size + atoi(nombre_parametros[1])),
+                        string_itoa(tarea_config.block_count+cantidad_de_bloques),
+                        bloques_config,
+                        "QWERTY"
+                    );
+                    config_save(tarea_config_file);
+                }
                 log_info(logger, "se genero %s unidades de comida",nombre_parametros[1]);
                 break;
             }
             case CONSUMIR_COMIDA:{
-                log_info(logger, "se consumio %s unidades de comida",nombre_parametros[1]);
+                printf("entro consumir\n");
+                char * path = crear_path_absoluto("/Files/Comida.ims");
+                tarea_config_file = config_create(path);
+                leer_tarea_config(tarea_config_file);
+                char ** lista_bloques = string_get_string_as_array(tarea_config.blocks);
+                printf("entro 1er if\n");
+                if(existe_archivo(path)){
+                    int size_a_modificar = atoi(nombre_parametros[1]);
+                    printf("entro 2do if\n");
+                    if(tarea_config.size > size_a_modificar){
+                        //todabia quedan caracteres
+                        int elem_restantes = tarea_config.size - size_a_modificar;
+
+                        int indice_restantes = elem_restantes/super_bloque.blocksize;
+                        int elem_restantes_resto = elem_restantes%super_bloque.blocksize;
+                        int bloques_a_modificar = tarea_config.block_count - indice_restantes;
+                        int cantidad_repetir;
+                        int block_config;
+                        if(elem_restantes_resto !=0){
+                            block_config = indice_restantes +1;
+                        }else{
+                            block_config = indice_restantes;
+                        }
+                        if (bloques_a_modificar>0){
+                            cantidad_repetir = size_a_modificar;
+                        }else{
+                            cantidad_repetir = super_bloque.blocksize-elem_restantes_resto;
+                        }
+
+                        if(elem_restantes_resto!=0){
+                            char * fill = string_repeat(' ',cantidad_repetir);
+                            int bloque_offset = atoi(lista_bloques[indice_restantes]) *super_bloque.blocksize;
+                            memcpy(blocksmap+bloque_offset+elem_restantes_resto,fill,strlen(fill));
+                            //size_a_modificar-=(cantidad_repetir);
+                            indice_restantes++;
+                        }
+                        printf("entro for bloques mod\n");
+                        for (int i = indice_restantes; lista_bloques[i]!=NULL; i++){
+                            printf("entro for bloques mod\n");
+                            char * fill = string_repeat(' ',super_bloque.blocksize);
+                            int offset = super_bloque.blocksize*atoi(lista_bloques[i]);
+                            memcpy(blocksmap+offset,fill,strlen(fill));
+                            printf("entro BITMAP\n");
+                            liberar_bit(&bitmap,i);
+                        }
+                        printf("sali del for \n");
+                        char * bloques_config = array_block_to_string(lista_bloques,block_config);
+                        msync(blocksmap, blocks_stat.st_size, MS_SYNC);
+                        printf("config\n");
+                        eliminar_keys_tarea(tarea_config_file);
+                        set_tarea_config(
+                            tarea_config_file,
+                            string_itoa(tarea_config.size - atoi(nombre_parametros[1])),
+                            string_itoa(block_config),
+                            bloques_config,
+                            "QWERTY"
+                        );
+                        config_save(tarea_config_file);
+                        log_info(logger, "se consumio %s unidades de comida",nombre_parametros[1]);
+                    }else{
+                        //borrar archivo entero
+                        for (int i = 0; lista_bloques[i]!=NULL; i++){
+                            printf("entro for bloques mod\n");
+                            char * fill = string_repeat(' ',super_bloque.blocksize);
+                            int offset = super_bloque.blocksize*atoi(lista_bloques[i]);
+                            memcpy(blocksmap+offset,fill,strlen(fill));
+                            printf("entro BITMAP\n");
+                            liberar_bit(&bitmap,i);
+                        }
+                        printf("sali del for \n");
+                        char * bloques_config = array_block_to_string(lista_bloques,0);
+                        msync(blocksmap, blocks_stat.st_size, MS_SYNC);
+                        printf("config\n");
+                        eliminar_keys_tarea(tarea_config_file);
+                        set_tarea_config(
+                            tarea_config_file,
+                            string_itoa(0),
+                            string_itoa(0),
+                            bloques_config,
+                            "QWERTY"
+                        );
+                        config_save(tarea_config_file);
+                        log_info(logger, "se intento consumir mas unidades de comida que las disponibles");
+                    }
+                    
+                }else{
+                    log_info(logger,"No se encontro archivo Comida.ims");
+                }
                 break;
             }
             case GENERAR_BASURA:{
+                char ** bloques_usables;
+                int cantidad_de_bloques;
+                char * path = crear_path_absoluto("/Files/Basura.ims");
+                
+                if(!existe_archivo(path)){
+                    //creando archivo Basura.ims de 0
+                    int resto = atoi(nombre_parametros[1])%super_bloque.blocksize;
+                    cantidad_de_bloques=atoi(nombre_parametros[1])/super_bloque.blocksize;
+                    if(resto!=0){
+                        cantidad_de_bloques++;
+                    }
+                    printf("cantidad bloques %d\n",cantidad_de_bloques);
+                    char * bloques_config;
+                    //string_append(&bloques_config,"[");
+                    crear_archivo(path);
+                    tarea_config_file = config_create(path);
+                    bloques_usables = bits_libres(&bitmap,cantidad_de_bloques);
+
+                    //modifica el array para guardarlo como char * en el config
+                    bloques_config = array_block_to_string(bloques_usables,cantidad_de_bloques);
+                    printf("bloques a config: %s\n", bloques_config);
+                    
+                    for(int i = 0; bloques_usables[i]!=NULL;i++){
+                        ocupar_bit(&bitmap, atoi(bloques_usables[i]));
+                        int offset = atoi(bloques_usables[i])*super_bloque.blocksize;
+                        if(i==cantidad_de_bloques-1){
+                            //string_append(&bloques_config,bloques_usables[i]);
+                            char * fill = string_repeat('B',resto);
+                            memcpy(blocksmap+offset,fill,resto);
+                        }else{
+                            //string_append(&bloques_config,bloques_usables[i]);
+                            //string_append(&bloques_config,",");
+                            
+                            char * fill = string_repeat('B',(int)super_bloque.blocksize);
+                            memcpy(blocksmap+offset,fill,super_bloque.blocksize);
+                        }
+                    }
+                    //string_append(&bloques_config,"]");
+                    msync(blocksmap, blocks_stat.st_size, MS_SYNC);
+                    config_set_value(tarea_config_file,"CARACTER_LLENADO","B");
+                    set_tarea_config(
+                        tarea_config_file,
+                        nombre_parametros[1],
+                        string_itoa(cantidad_de_bloques),
+                        bloques_config,
+                        "QWERTY"
+                    );
+                    config_save(tarea_config_file);
+                    printf("caracter llenado: %s\n",config_get_string_value(tarea_config_file,"CARACTER_LLENADO"));
+                }else{
+                    //Ya existiendo Basura.ims
+                    tarea_config_file = config_create(path);
+                    leer_tarea_config(tarea_config_file);
+                    char * caracter_llenado = tarea_config.caracter_llenado;
+                    char ** lista_bloques = string_get_string_as_array(tarea_config.blocks);
+                    char * bloques_config;
+                    int size_a_modificar = atoi(nombre_parametros[1]);
+
+                    int resto_anterior = tarea_config.size%super_bloque.blocksize;
+                    printf("resto anterior%d\n",resto_anterior);
+                    int cantidad_repetir;
+
+                    //En caso de que el ultimo bloque no este lleno, decide si 
+                    //hay que usar solo parte de el o
+                    //utilizar todo el bloque y solicitar otro
+                    if((super_bloque.blocksize-resto_anterior)>size_a_modificar){
+                        cantidad_repetir = size_a_modificar;
+                    }else{
+                        cantidad_repetir = super_bloque.blocksize-resto_anterior;
+                    }
+                    if(resto_anterior!=0){
+                        char * fill = string_repeat(*caracter_llenado,cantidad_repetir);
+                        char * ultimo_bloque_anterior = lista_bloques[tarea_config.block_count-1];
+                        printf("bloque numero:%s\n",ultimo_bloque_anterior);
+                        int ultimo_bloque_offset = atoi(ultimo_bloque_anterior)*super_bloque.blocksize;
+                        memcpy(blocksmap+ultimo_bloque_offset+ resto_anterior,fill,cantidad_repetir);
+                        size_a_modificar-=(cantidad_repetir);
+                    }
+
+                    int resto = size_a_modificar%super_bloque.blocksize;
+                    printf("resto actual:%d\n",resto);
+                    cantidad_de_bloques=size_a_modificar/super_bloque.blocksize;
+                    if(resto!=0){
+                        cantidad_de_bloques++;
+                    }
+                    printf("cantidad bloques %d\n",cantidad_de_bloques);
+                   
+                    //solicita una lista de bloques disponibles y los setea en memoria
+                    bloques_usables = bits_libres(&bitmap,cantidad_de_bloques);
+
+                    //une la lista de bloques del config con los bloques nuevos a usar
+                    bloques_config=array_two_block_to_string(lista_bloques,bloques_usables);
+                    printf("bloques a config: %s\n", bloques_config);
+
+                    for(int i = 0; bloques_usables[i]!=NULL;i++){
+                        int offset = atoi(bloques_usables[i])*super_bloque.blocksize;
+                        if(i==cantidad_de_bloques-1){
+                            char * fill = string_repeat(*caracter_llenado,resto);
+                            memcpy(blocksmap+offset,fill,strlen(fill));
+                        }else{
+                            char * fill = string_repeat(*caracter_llenado,(int)super_bloque.blocksize);
+                            memcpy(blocksmap+offset,fill,strlen(fill));
+                        }
+                    }
+                    msync(blocksmap, blocks_stat.st_size, MS_SYNC);
+                    eliminar_keys_tarea(tarea_config_file);
+                    set_tarea_config(
+                        tarea_config_file,
+                        string_itoa(tarea_config.size + atoi(nombre_parametros[1])),
+                        string_itoa(tarea_config.block_count+cantidad_de_bloques),
+                        bloques_config,
+                        "QWERTY"
+                    );
+                    config_save(tarea_config_file);
+                }
                 log_info(logger, "se genero %s unidades de basura",nombre_parametros[1]);
                 break;
             }
             case DESCARTAR_BASURA:{
-                log_info(logger, "se descarto la basura");
+                char * path = crear_path_absoluto("/Files/Basura.ims");
+                
+                if(existe_archivo(path)){
+                    remove(path);
+                    log_info(logger, "Se descarto la basura");
+                }else{
+                    log_info(logger, "Se intento descartar basura pero no existe el archivo Basura.ims");
+                }
+                
                 break;
             }
             default:{
