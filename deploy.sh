24703e7ccc92dbd5bb2eef0923751e4355c2ccd7
juan-nisimura <juanisimura@gmail.com>
2021-07-21 18:18:55 -0300

    modifico deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,15 @@ length=$(($#-1))
 OPTIONS=${@:1:$length}
 REPONAME="${!#}"
 CWD=$PWD
-#echo -e "\n\nInstalling commons libraries...\n\n"
-#COMMONS="so-commons-library"
-#git clone "https://github.com/sisoputnfrba/${COMMONS}.git" $COMMONS
-#cd $COMMONS
-#sudo make uninstall
-#make all
-#sudo make install
-#cd $CWD
+
+echo -e "\n\nInstalling commons libraries...\n\n"
+COMMONS="so-commons-library"
+git clone "https://github.com/sisoputnfrba/${COMMONS}.git" $COMMONS
+cd $COMMONS
+sudo make uninstall
+make all
+sudo make install
+cd $CWD
 
 # Biblioteca para dibujar el mapa (revisar)
 git clone https://github.com/sisoputnfrba/so-nivel-gui-library/
@@ -24,7 +25,6 @@ cd $CWD
 cd Discordiador
 git clone https://github.com/sisoputnfrba/a-mongos-pruebas.git
 cd $CWD
-
 
 echo -e "\n\nBuilding projects...\n\n"
 cd $PWD
